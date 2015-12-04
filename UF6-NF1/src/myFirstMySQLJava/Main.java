9251d462e48bc813f69df713fbc839d1f9afb2db
ismafel <iaw188216@h11.informatica.escoladeltreball.org>
2015-12-04 18:26:27 +0100

    +avances

--- UF6-NF1/src/myFirstMySQLJava/Main.java
+++ UF6-NF1/src/myFirstMySQLJava/Main.java
@@ -3,6 +3,8 @@ package myFirstMySQLJava;
 public class Main {
 
 	public static void main(String[] args) {
+		MySQLAccess dao = new MySQLAccess();
+		dao.readDataBase();
 	}
 
 }
--- UF6-NF1/src/myFirstMySQLJava/MySQLAccess.java
+++ UF6-NF1/src/myFirstMySQLJava/MySQLAccess.java
@@ -3,22 +3,68 @@ package myFirstMySQLJava;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class MySQLAccess {
 
 	private Connection connect = null;
-	private Statement statement= null;
-	private ResultSet result = null;
-	
+	private Statement statement = null;
+	private ResultSet resultSet = null;
+
 	public void readDataBase() {
-		String driver ="com.mysql.jdbc.Driver";
+		String driver = "com.mysql.jdbc.Driver";
 		try {
 			Class.forName(driver);
-		//	connect = DriverManager.getConnection(url);
+			connect = DriverManager
+					.getConnection("jdbc:mysql://localhost/feedback?" + "user=sqluser&password=sqluserpw");
+			statement = connect.createStatement();
+
+			resultSet = statement.executeQuery("select * from feedback.comments");
+			writeResultSet(resultSet);
+
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	private void writeResultSet(ResultSet resultSet2) throws SQLException {
+		while (resultSet.next()) {
+			// It is possible to get the columns via name
+			// also possible to get the columns via the column number
+			// which starts at 1
+			// e.g. resultSet.getSTring(2);
+			String user = resultSet.getString("myuser");
+			String website = resultSet.getString("webpage");
+			String summary = resultSet.getString("summary");
+			Date date = resultSet.getDate("datum");
+			String comment = resultSet.getString("comments");
+			System.out.println("User: " + user);
+			System.out.println("Website: " + website);
+			System.out.println("Summary: " + summary);
+			System.out.println("Date: " + date);
+			System.out.println("Comment: " + comment);
+		}
+	}
+
+	private void close() {
+		try {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			if (statement != null) {
+				statement.close();
+			}
+			if (connect != null) {
+				connect.close();
+			}
+		} catch (Exception e) {
+		}
+	}
+
 }
